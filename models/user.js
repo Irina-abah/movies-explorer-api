7b74ac5980a09ae2dfc395c8459dc0f0082044e0
Irina Abah <irin.bon@gmail.com>
2021-06-02 23:46:11 +0100

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const { isEmail } = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, 'Incorrect email'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+    default: null,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Username',
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+module.exports = mongoose.model('user', userSchema);
